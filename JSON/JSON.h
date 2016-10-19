27f57b21202d2a019e0ee32bcc66fad4e6847800
Mark Onyschuk <monyschuk@zynga.com>
2016-10-19 10:00:16 -0400

    Initial code commit

---
+++ JSON/JSON.h
@@ -0,0 +1,19 @@
+//
+//  JSON.h
+//  JSON
+//
+//  Created by monyschuk on 10/19/16.
+//  Copyright © 2016 Mark Onyschuk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for JSON.
+FOUNDATION_EXPORT double JSONVersionNumber;
+
+//! Project version string for JSON.
+FOUNDATION_EXPORT const unsigned char JSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSON/PublicHeader.h>
+
+
